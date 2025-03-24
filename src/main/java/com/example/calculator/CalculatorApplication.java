a866ed2998a0e1ffd36c49f441afa1d7d557c070
eagle991 <eagle991@mail.ru>
2025-03-24 21:12:49 +0300

    Введение в Maven и Spring Boot

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculatorApplication {
+	public CalculatorApplication() {
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/calculator/calculator/CalculatorController.java
+++ src/main/java/com/example/calculator/calculator/CalculatorController.java
@@ -1,6 +1,52 @@
-package com.example.calculator.calculator;
+package com.example.calculator.controller;
 
+import com.example.calculator.service.CalculatorService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"/calculator"})
 public class CalculatorController {
+    private final CalculatorService calculatorService;
 
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
 
+    @GetMapping
+    public String welcome() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    @GetMapping({"/plus"})
+    public String add(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
+        this.validateParams(num1, num2);
+        return this.calculatorService.add(num1, num2);
+    }
+
+    @GetMapping({"/minus"})
+    public String subtract(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
+        this.validateParams(num1, num2);
+        return this.calculatorService.subtract(num1, num2);
+    }
+
+    @GetMapping({"/multiply"})
+    public String multiply(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
+        this.validateParams(num1, num2);
+        return this.calculatorService.multiply(num1, num2);
+    }
+
+    @GetMapping({"/divide"})
+    public String divide(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
+        this.validateParams(num1, num2);
+        return this.calculatorService.divide(num1, num2);
+    }
+
+    private void validateParams(Integer num1, Integer num2) {
+        if (num1 == null || num2 == null) {
+            throw new IllegalArgumentException("Both parameters must be provided.");
+        }
+    }
 }
