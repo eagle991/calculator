95cb90a6a008ab0a4d52fbb45496414755a6f795
eagle991 <eagle991@mail.ru>
2025-03-24 21:10:11 +0300

    Введение в Maven и Spring Boot

---
+++ src/main/java/com/example/calculator/calculator/CalculatorController.java
@@ -0,0 +1,6 @@
+package com.example.calculator.calculator;
+
+public class CalculatorController {
+
+
+}
---
+++ src/main/java/com/example/calculator/model/CalculationResult.java
@@ -0,0 +1,4 @@
+package com.example.calculator.model;
+
+public class CalculationResult {
+}
